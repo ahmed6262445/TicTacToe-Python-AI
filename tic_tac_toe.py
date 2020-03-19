5d694b1e7349b7f10f75f25dd94abfcbbe40e576
Ahmed <01-131172-004@student.bahria.edu.pk>
2020-03-19 22:43:31 +0500

    Added Player Functions

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,9 +1,13 @@
-class TicTacToe():
-    def __init__(self):
+import random
+class Board():
+    def __init__(self,last_player='Player 1'):
+        self.current_player = last_player
+        self.player_1 = 'Player 1'
+        self.player_2 = 'Player 2'
         self.board = [
-            ['x','o','x'],
-            ['o','x','o'],
-            ['o','x','x']
+            ['1','2','3'],
+            ['4','5','6'],
+            ['7','8','9']
         ]
     
     def print_board(self):
@@ -18,11 +22,35 @@ class TicTacToe():
         for i in range(3):
             for j in range(3):
                 board += self.board[i][j]
-                board += " | "
+                if j != 2:
+                    board += " | "
             board += "\n"
         return board
 
+    ########################### Player Functions ###########################
+    def shift_player(self):
+        if self.current_player == self.player_1:
+            self.current_player = self.player_2
+        else:
+            self.current_player = self.player_1
 
+    def get_current_player(self):
+        """
+            returns current player
+        """
+        return self.current_player
+    ########################### Player Functions End ###########################
 
-obj = TicTacToe()
+    
+
+obj = Board()
 print( obj.print_board())
+
+
+a = obj.get_current_player()
+print( a)
+obj.shift_player()
+
+a = obj.get_current_player()
+print( a)
+obj.shift_player()
