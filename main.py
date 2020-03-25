1b31c9ff54fe0e2accf157dd58470cfb3666881e
Ahmed <01-131172-004@student.bahria.edu.pk>
2020-03-25 23:09:05 +0500

    Added graph

--- main.py
+++ main.py
@@ -1,14 +1,58 @@
 from tic_tac_toe import TicTacToe
 from os import system
-import getch as inp
 from time import sleep
 import colors
-clear = lambda: system('clear')
+import getch as inp
+import platform
+from adjacency_matrix_graph import AdjacencyMatrixGraph
+from depth_first_search import *
+from breadth_first_search import *
+from os import system
+
+def clear():
+    """
+    Clears the console
+    """
+    os_name = platform.system().lower()
+    if 'windows' in os_name:
+        system('cls')
+    else:
+        system('clear')
+
+graph = AdjacencyMatrixGraph(9)
+
+graph.add_edge(0,1)
+graph.add_edge(0,3)
+graph.add_edge(0,4)
+
+graph.add_edge(1,2)
+graph.add_edge(1,4)
+
+graph.add_edge(2,4)
+graph.add_edge(2,5)
+
+graph.add_edge(3,4)
+graph.add_edge(3,6)
+
+graph.add_edge(4,5)
+graph.add_edge(4,6)
+graph.add_edge(4,7)
+graph.add_edge(4,8)
+
+graph.add_edge(5,8)
+
+graph.add_edge(6,7)
+
+graph.add_edge(7,8)
+
+# depth_first(graph)
+# print("Breadth First Search")
+# breadth_first(graph)
+# exit()
 
 player_1 = 'O'
 player_2 = 'X'
 plays_first = 'O'
-
 clear()
 print("Loading...")
 sleep(1)
